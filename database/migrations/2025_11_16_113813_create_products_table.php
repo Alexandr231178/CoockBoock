c2de7c55a134dfd525a4284955ce3220ddcb3b1a
Alrxandr <alexandr9652764@gmail.com>
2025-11-16 16:44:52 +0500

    Deleted products group

--- database/migrations/2025_11_16_113813_create_products_table.php
+++ database/migrations/2025_11_16_113813_create_products_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('id_group');
-            $table->string('description');
             $table->timestamps();
         });
     }
