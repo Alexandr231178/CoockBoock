41fc1918bac23db5fd916aad1d725046c0eb554c
Alrxandr <alexandr9652764@gmail.com>
2025-11-16 17:36:29 +0500

    changed tables sete_cjmponents and dishes_components

--- app/Models/Products/Products.php
+++ app/Models/Products/Products.php
@@ -4,17 +4,12 @@ namespace App\Models\Products;
 
 use App\Models\Dishes\DishesComponent;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 
 class Products extends Model
 {
     protected $fillable = ['name'];
-    public function products_groups(): belongsTo
-    {
-        return $this->belongsTo(ProductsGroup::class);
-    }
 
     public function dish_components(): HasMany
     {
