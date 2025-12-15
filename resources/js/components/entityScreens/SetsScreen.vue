60a048a15c39ae184cbdda14661dc38e56fe1a67
Alrxandr <alexandr9652764@gmail.com>
2025-12-15 21:24:13 +0500

    next step create function update sets

--- resources/js/components/entityScreens/SetsScreen.vue
+++ resources/js/components/entityScreens/SetsScreen.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { useSets } from "../../stores/sets.js";
 import { useSetsComponent } from "../../stores/setsComponent.js";
+import { useDishes } from "../../stores/dishes.js";
 import TextButtonAlfa from "../buttons/TextButtonAlfa.vue";
 import Cange from "../icons/Cange.vue";
 import Delete from "../icons/Delete.vue";
 import {ref, toRaw, watch} from "vue";
+import AtherInputs from "../Inputs/AtherInputs.vue";
+import H2 from "../titles/H2.vue";
+import Arrow from "../icons/Arrow.vue";
 
 const useStoreSets =useSets();
 useStoreSets.getSets();
@@ -13,6 +17,14 @@ const setsList = ref(useStoreSets.sets);
 const useStoreSetsComponent = useSetsComponent();
 useStoreSetsComponent.getSetsComponent();
 const setsComponentList = ref(useStoreSetsComponent.setsComponent);
+
+const useStoreDishes = useDishes();
+useStoreDishes.getDishes();
+const dishes = ref(useStoreDishes.dishes);
+
+const updateSetName = ref();
+const updateDescription = ref();
+let updateDishesList = ref([]);
 
 watch(
     () => useStoreSets.sets,
@@ -28,6 +40,18 @@ watch(
     }
 )
 
+watch(
+    () => useStoreDishes.dishes,
+    () => {
+        dishes.value = useStoreDishes.dishes;
+    }
+)
+
+//Функция изменяющая стол
+function updateSet() {
+    pass
+}
+
 //Функция которая отбирает из списка компонентов только компоненты выбранного стола и складывает их в массив
 function createChangeComponentList(setsComponentList, id) {
     let result = [];
@@ -36,20 +60,31 @@ function createChangeComponentList(setsComponentList, id) {
         let res = {};
         res['id'] = element.id;
         res['sets_of_dishes_id'] = element.sets_of_dishes_id;
+        res['dishes_id'] = element.dishes_id;
         result.push(res);
     })
     return result
 }
 
 function changeFunction(id) {
-    console.log('Вызвана функция редактирования', id)
-    const setsComponentChangeList = createChangeComponentList(setsComponentList, id);
-    console.log(setsComponentChangeList);
+    //console.log(setsComponentList.value);
+    updateDishesList.value = createChangeComponentList(setsComponentList, id);
+    updateDishesList.value.forEach((element) => {
+        dishes.value.forEach((el)=> {
+            if(element.dishes_id === el.id) {
+                element['dishesName'] = el.name;
+            }
+        })
+    })
+    const changeSet = setsList.value.filter(item => item.id === id);
+    updateSetName.value = changeSet[0].name;
+    updateDescription.value = changeSet[0].description;
 
+    console.log(updateDishesList.value);
 }
 
 function deleteFunction(id) {
-    console.log('Вызвана функция удаления', id)
+    //console.log('Вызвана функция удаления', id)
     useStoreSets.deleteSet(id);
     useStoreSets.getSets();
     const setsComponentChangeList = createChangeComponentList(setsComponentList, id);
@@ -58,18 +93,63 @@ function deleteFunction(id) {
     })
     useStoreSetsComponent.getSetsComponent();
 }
+
+function deleteDishFunction(id) {
+    console.log('Вызвана функция удаления', id)
+    //updateDishesList.value = updateDishesList.value.pop(id)
+    updateDishesList.value = updateDishesList.value.filter(item => item.id !== id);
+}
+
+function addDishFunction(id) {
+    let foundObject = dishes.value.find(obj => obj.id === id);
+    const found = updateDishesList.value.find(item => item.dishesName === foundObject.name);
+    if (!found) {
+        let resultFoundObject = {id: null,
+            sets_of_dishes_id: updateDishesList.value[0].sets_of_dishes_id,
+            dishes_id: foundObject.id,
+            dishesName: foundObject.name}
+        updateDishesList.value.push(resultFoundObject);
+    }
+    console.log(updateDishesList.value);
+}
+
+
+
+
 </script>
 
 <template>
     <div class="container">
-        <div class="rounded-2xl bg-white p-4">
-            <div class="mb-1 flex gap-1" v-for="set in setsList" :key="set.id">
-                <TextButtonAlfa>{{ set.name }}</TextButtonAlfa>
-                <button @click="changeFunction(set.id)"><Cange></Cange></button>
-                <button @click="deleteFunction(set.id)"><Delete></Delete></button>
+        <div class="rounded-2xl bg-white p-4 flex">
+            <div class="mr-4 border-e-4 pr-4">
+                <div class="mb-1 flex gap-1" v-for="set in setsList" :key="set.id">
+                    <TextButtonAlfa>{{ set.name }}</TextButtonAlfa>
+                    <button @click="changeFunction(set.id)"><Cange></Cange></button>
+                    <button @click="deleteFunction(set.id)"><Delete></Delete></button>
+                </div>
             </div>
-            <div>
-<!--                Здесь будет окно редактирования-->
+            <div class="border-e-4 mr-4 pr-4">
+                <H2>Окно редактирования стола</H2>
+                <div>
+                    <AtherInputs v-model="updateSetName" placeholder="Здесь будет имя выбранного стола" class="mb-2">
+                    </AtherInputs>
+                    <AtherInputs v-model="updateDescription" placeholder="Здесь будет описание стола" class="mb-2">
+                    </AtherInputs>
+                    <div v-for="dish in updateDishesList" :key="dish.id" class="mb-4 flex gap-1">
+                        <TextButtonAlfa>{{ dish.dishesName }}</TextButtonAlfa>
+                        <button @click="deleteDishFunction(dish.id)"><Delete></Delete></button>
+                    </div>
+                    <TextButtonAlfa>Изменить</TextButtonAlfa>
+                </div>
+            </div>
+            <div class="mr-4 pr-4">
+                <H2 class="mb-4">Окно добавления блюд</H2>
+                <div>
+                    <div v-for="dish in dishes" :key="dish.id" class="flex mb-2">
+                        <Arrow @click="addDishFunction(dish.id)" class="mr-1 rotate-180"></Arrow>
+                        <TextButtonAlfa class="">{{ dish.name }}</TextButtonAlfa>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
